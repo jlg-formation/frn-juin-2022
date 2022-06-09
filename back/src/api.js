caf25ea56c01af519aba5fb5f812f53c6cd05b46
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-09 15:07:10 +0200

    dev: connexion ok

--- back/src/api.js
+++ back/src/api.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express.Router();
 
+app.use(express.json());
+
 app.post("/connect", (req, res) => {
-  res.status(401).end();
+  const loginForm = req.body;
+  console.log("loginForm: ", loginForm);
+  if (loginForm.login !== "admin") {
+    res.status(401).end();
+    return;
+  }
+  res.json({
+    displayName: "Admin Orsys",
+  });
 });
 
 module.exports = app;
